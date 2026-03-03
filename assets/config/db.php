1153d00231bdd68de4eb53ff4555bbcb2960bbd8
Sara Hörvallius <sara.horvallius0134@stud.hkr.se>
2026-03-03 17:05:46 +0000

    uppdaterat php-filer i mappar

---
+++ assets/config/db.php
@@ -0,0 +1,6 @@
+/* Fyra funktioner */
+CRUD
+Create - Skapa infromation          = INSERT INTO 
+Read - Läsa/hämta infromation       = SELECT
+Update - Uppdatera information      = UPDATE
+Delete - Radera infromation         = DELETE
