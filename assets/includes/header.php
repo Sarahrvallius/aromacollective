14536fbdaeed4d57c77323d056f0ca76c90d34f0
Lina Hagman <lina.hagman0319@stud.hkr.se>
2026-03-04 11:49:39 +0000

    lagt till font i header + css

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
-
-
+<html lang="eng">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
     <title>Aroma Collective</title>
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css"> <!-- Länk till CSS-fil -->
+    <!--Bootstrap CSS-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!--Font Awesome CSS-->
+    <link rel="stylesheet" href="assets/css/all.min.css">
+    <!--Custom CSS-->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!--Google Fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:ital,opsz,wght@0,6..96,400..900;1,6..96,400..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
 </head>
-
-
-</body>
-
 </html>
--- profile.php
+++ profile.php
@@ -1,20 +1,11 @@
-<!DOCTYPE html>
-<html lang="eng">
-
-<head>
-    <meta charset="utf-8">
-    <title>Aroma Collective</title>
-    <!--Bootstrap CSS-->
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-    <!--Font Awesome CSS-->
-    <link rel="stylesheet" href="assets/css/all.min.css">
-    <!--Custom CSS-->
-    <link rel="stylesheet" href="assets/css/style.css">
-    <!--Google Fonts-->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:ital,opsz,wght@0,6..96,400..900;1,6..96,400..900&family=Old+Standard+TT:ital,wght@0,400;0,700;1,400&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-</head>
+<?php
+// show errors for debugging
+require_once 'assets/includes/display_errors.php';
+// inkluderar header
+require_once 'assets/includes/header.php';
+//includes database connection
+require_once 'assets/config/db.php';
+?>
 
 <body>
     <main>
@@ -49,5 +40,3 @@
         </section>
     </main>
 </body>
-
-</html>
