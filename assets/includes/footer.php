616dcf103ebed6f46fd811d88de7020745c45437
Lina Hagman <lina.hagman0319@stud.hkr.se>
2026-03-04 16:21:32 +0000

    modifierat struktur på header+footer

--- assets/includes/footer.php
+++ assets/includes/footer.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Aroma Collective</title>
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css"> <!-- Länk till CSS-fil -->
-</head>
+<footer>
+</footer>
 
-
-
-
-
-
+</body>
 </html>
--- assets/includes/header.php
+++ assets/includes/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="eng">
+
 <head>
     <meta charset="utf-8">
     <title>Aroma Collective</title>
@@ -14,4 +15,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Bodoni+Moda:ital,opsz,wght@0,6..96,400..900;1,6..96,400..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
 </head>
-</html>
+
+<body>
+
+    <header>
+    </header>
