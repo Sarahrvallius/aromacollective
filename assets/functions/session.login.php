c110015b2e9bd565c69fd251273b0e4a0df60030
Sara Hörvallius <sara.horvallius0134@stud.hkr.se>
2026-03-19 13:12:03 +0000

    skickas till profile när man loggat in

--- assets/functions/session.login.php
+++ assets/functions/session.login.php
@@ -34,8 +34,8 @@ if (isset($_POST['login'])) {
         $row = $stmt->fetch();
         //creates session variable with user id
         $_SESSION['user_id'] = $row['id'];
-        //Redirect to index.php with success message
-        header('Location: ../../index.php?action=success');
+        //Redirect to profile.php with success message
+        header('Location: ../../profile.php?action=success');
         exit();
     } else {
         //redirect to index.php with error message if user does not exist
