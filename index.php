f900ce3d0c771ab89f9cfe91f89dd7bbb3ee46b2
Sara Hörvallius <sara.horvallius0134@stud.hkr.se>
2026-03-23 10:25:34 +0000

    raderat filer - lagt till notifications

--- index.php
+++ index.php
@@ -2,48 +2,10 @@
 // require_once 'assets/config/db.php'; // avkommentera när databasen funkar
 require_once 'assets/includes/header.php'; // includes header
 require_once 'assets/functions/select-top-perfumes.php';
+require_once 'assets/functions/notifications.php';
+
 ?>
 
-<!-- SUCCESS OR ERROR MESSAGES when logging in -->
-<?php //message if login successful
-// check if an action is set
-if (isset($_GET['action'])) {
-
-    //check wich action is set
-    switch ($_GET['action']) {
-        case 'success':
-
-            // display success message
-            echo '<div class="alert alert-success text-center mb-0" role="alert">
-                        You have successfully logged in!
-                      </div>';
-            break;
-
-        case 'error':
-
-            // display error message
-            echo '<div class="alert alert-danger text-center mb-0" role="alert">
-                        Incorrect email or password. Please try again.
-                      </div>';
-            break;
-
-        case 'empty':
-
-            // display error message
-            echo '<div class="alert alert-warning text-center mb-0" role="alert">
-                        Please fill in both email and password fields.
-                      </div>';
-            break;
-
-        case 'logout':
-            // display logout message
-            echo '<div class="alert alert-info text-center mb-0" role="alert">
-                        You have been logged out.
-                      </div>';
-            break;
-    }
-}
-?>
 
 <main class="bg-offwhite">
     <!-- Hero section-->
